a7ab25fe3bb86081a9593534123426fe659431ba
Jack <peppyjack2000@gmail.com>
2020-04-14 21:07:38 -0400

    Final Push

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports =
 {
    // Local host connection
-   // "URI":"mongodb://localhost/mydb" 
+   "URI":"mongodb://localhost/mydb" ,
    // Online Connection
     "URI":"mongodb+srv://jack:piggy@cluster0-c1rpz.mongodb.net/test?retryWrites=true&w=majority"
 }
